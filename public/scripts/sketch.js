ff27e2957f914841ca6b29d2964c44078ca1059f
prathgan <gandhips02@gmail.com>
2020-07-14 17:42:11 -0400

    remove drawing functions

--- public/scripts/sketch.js
+++ public/scripts/sketch.js
@@ -18,25 +18,6 @@ function setup(){
 
 }
 
-function newDrawing(data){
-	noStroke();
-	fill(255, 0, 100);
-	ellipse(data.x, data.y, 36, 36);
-}
-
-function mouseDragged(){
-
-	var data = {
-		x: mouseX,
-		y: mouseY
-	}
-	socket.emit('mouse', data);
-
-	noStroke();
-	fill(255)
-	ellipse(mouseX, mouseY, 36, 36);
-}
-
 function draw(){
 	background(220);
 	ellipse(x, y, 50, 50)
