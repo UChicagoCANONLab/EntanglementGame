9ffb831ec6bc79185015c18cc2e9ec997d73ad82
prathgan <gandhips02@gmail.com>
2020-07-26 17:09:43 -0400

    fromat

--- public/scripts/sketch.js
+++ public/scripts/sketch.js
@@ -346,9 +346,9 @@ function preload() {
 function setup(){
 
 	var canvasDiv = document.getElementById('game_stage');
-  var width = canvasDiv.offsetWidth;
-  //var game_canvas = createCanvas(width,600);
-  var game_canvas = createCanvas(648,648);
+  	var width = canvasDiv.offsetWidth;
+  	//var game_canvas = createCanvas(width,600);
+  	var game_canvas = createCanvas(648,648);
 	game_canvas.parent("game_stage");
 
 	background(maze_img);
