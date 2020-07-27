71c9bc959a40de05914d4532a5182296d8fc264d
tovarm-uchi <56130057+tovarm-uchi@users.noreply.github.com>
2020-07-27 12:56:50 -0500

    changing the LevelOver to socket.to instead of io.in

--- server.js
+++ server.js
@@ -72,7 +72,7 @@ function newConnection(socket){
 
 	function sendLevelOver(data){
 		console.log("telling clients level is over")
-		io.in(data.gameID).emit('LevelOver', data.complete)
+		socket.to(data.gameID).emit('LevelOver', data.complete)
 	}
 
 	function joinGame(data){
